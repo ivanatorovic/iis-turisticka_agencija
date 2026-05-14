e092d1820c157eac78df3e877c8116449aa4a668
ivanatorovic <ivana.torovic2003@gmail.com>
2026-05-14 15:55:01 +0200

    Initial project setup

---
+++ turisticka-agencija/src/main/java/com/example/turisticka_agencija/TuristickaAgencijaApplication.java
@@ -0,0 +1,13 @@
+package com.example.turisticka_agencija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristickaAgencijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristickaAgencijaApplication.class, args);
+	}
+
+}
